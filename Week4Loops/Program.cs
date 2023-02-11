78fc8ac5966520d3456d67132c40a2f2eee273f5
eemottus <emottus@tktk.eegit config --global user.name eemottus git config --global user.email emottus@tktk.ee>
2023-02-11 11:19:45 +0200

    week4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//for loop
+//DRY-principle - Do not Repeat Yourself
+
+int i = 0;
+
+for (int i = 0; i < 5; i++) //i++ = i + 1
+{
+    Console.WriteLine("We don't need no education.");
+}
+
+Console.WriteLine(i);
+Console.WriteLine("Have a nice day!");
